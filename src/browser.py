40307a38189bb514ecbc4eebeb8f31d20b27e1cb
arishalidev <hello@arishali.dev>
2026-06-13 16:16:59 -0500

    implemented scrolling

--- src/browser.py
+++ src/browser.py
@@ -1,5 +1,9 @@
 import tkinter
+
 WIDTH, HEIGHT = 800, 600
+H_STEP, V_STEP = 13, 18
+SCROLL_STEP = 100
+
 
 def lex(body):
     in_tag = False
@@ -15,6 +19,8 @@ def lex(body):
 
 class Browser:
     def __init__(self):
+        self.display_list = None
+        self.scroll = 0
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
             self.window,
@@ -23,16 +29,34 @@ class Browser:
         )
         self.canvas.pack()
 
+        self.window.bind("<Down>", self.scrolldown)
+
+
     def load(self, url):
         body = url.request()
         text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
 
-        HSTEP, VSTEP = 13, 18
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
 
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_y += VSTEP
-                cursor_x = HSTEP
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = H_STEP, V_STEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += H_STEP
+
+        if cursor_x >= WIDTH - H_STEP:
+            cursor_y += V_STEP
+            cursor_x = H_STEP
+
+    return display_list
